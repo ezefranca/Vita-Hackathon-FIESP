e900e4c67d5055d2e010c23f6e1d90124658fbb7
Ezequiel - OSX <ezequiel.ifsp@gmail.com>
2014-04-26 20:17:14 -0300

    Estrutura Inicial do Projeto

---
+++ Vita/TelaVC.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Vita
+//
+//  Created by Ezequiel Franca dos Santos on 26/04/14.
+//  Copyright (c) 2014 Ezequiel Franca dos Santos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TelaVC : UITabBarController
+
+@end
---
+++ Vita/TelaVC.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Vita
+//
+//  Created by Ezequiel Franca dos Santos on 26/04/14.
+//  Copyright (c) 2014 Ezequiel Franca dos Santos. All rights reserved.
+//
+
+#import "TelaVC.h"
+
+@interface TelaVC ()
+
+@end
+
+@implementation TelaVC
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
